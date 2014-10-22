9d2125daf922188a0902630c3f7678624fe0b7d6
drinkingkazu <kazuhiro@nevis.columbia.edu>
2014-10-22 06:53:26 -0500

    Example repository --kazu

---
+++ EmptyExample/sample.cxx
@@ -0,0 +1,6 @@
+#ifndef LARLITE_EMPTYEXAMPLE_SAMPLE_CXX
+#define LARLITE_EMPTYEXAMPLE_SAMPLE_CXX
+
+#include "sample.h"
+
+#endif
---
+++ EmptyExample/sample.h
@@ -0,0 +1,38 @@
+/**
+ * \file sample.h
+ *
+ * \ingroup EmptyExample
+ * 
+ * \brief Class def header for a class sample
+ *
+ * @author kazuhiro
+ */
+
+/** \addtogroup EmptyExample
+
+    @{*/
+#ifndef LARLITE_EMPTYEXAMPLE_SAMPLE_H
+#define LARLITE_EMPTYEXAMPLE_SAMPLE_H
+
+#include <iostream>
+
+/**
+   \class sample
+   User defined class EmptyExample ... these comments are used to generate
+   doxygen documentation!
+ */
+class sample{
+
+public:
+
+  /// Default constructor
+  sample(){}
+
+  /// Default destructor
+  virtual ~sample(){};
+
+};
+
+#endif
+/** @} */ // end of doxygen group 
+
